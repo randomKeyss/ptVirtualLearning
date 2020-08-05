bf9edf3dfcc5e993eaa218d022c4d4134a2f3107
randomKeyss <43837847+randomKeyss@users.noreply.github.com>
2020-08-05 11:13:14 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,15 +1,41 @@
+
+
+
+
 function successFunc(data) {
+    data = data.sheet1;
     console.log(data);
+    console.log(data[0].isTutoring);
     for(var i = 0; i<data.length;i++){
         
-        if(data[i]["isTutoring"] === "TRUE"){
+        if(data[i] && data[i].isTutoring){
+            console.log(i);
             var b = document.createElement("button");
-            b.innerText = data[i]["Subject"].split().join("&nbsp");
-            b.setAttribute("onclick", `location.href='${data[i]["Zoom Link"]}'`);
+            b.innerText = data[i].subject.split().join("&nbsp");
+            b.setAttribute("onclick", `location.href='${data[i].zoomLink}'`);
             b.setAttribute("type", "button");
             document.querySelectorAll("div")[1].appendChild(b);
         }
     }
+}
+
+console.log("HIHIHIHHIIMDAVID")
+var request = new XMLHttpRequest();
+request.open('GET', 'https://v2-api.sheety.co/b0b9b67f97e5f0def33bc752f16ead7d/ptVirtualTutoring/sheet1', true);
+
+request.onload = function() {
+  if (request.status >= 200 && request.status < 400) {
+    // Success!
+    var data = JSON.parse(request.responseText);
+    successFunc(data);
+  } else {
+    // We reached our target server, but it returned an error
+
   }
-  console.log("HIHIHIHHIIMDAVID")
-  Sheetsu.read("https://sheetsu.com/apis/v1.0su/ea46ac1c53de", {}, successFunc);
+};
+
+request.onerror = function() {
+  // There was a connection error of some sort
+};
+
+request.send();
